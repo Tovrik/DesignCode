6494ffdedc5604c97891ed83771e025aa0ffc880
Tovrik <stefan.t.peterson@gmail.com>
2020-09-21 17:18:11 -0700

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Stefan Peterson on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
